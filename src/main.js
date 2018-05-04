6d4f41d9ad96b021cd1c1e0c2fde5d7fd2d84866
13011009495 <gaojingzhe@yunzujia.com>
2018-05-04 19:18:56 +0800

    add interceptors for axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import iView from 'iview';
 import {router} from './router/index';
-import {appRouter} from './router/router';
+import {appRouter, loginRouter, page500} from './router/router';
 import store from './store';
 import App from './app.vue';
 import 'iview/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
 import util from './libs/util';
+import Cookies from 'js-cookie';
 
 Vue.use(VueI18n);
 Vue.use(iView);
@@ -39,5 +40,33 @@ new Vue({
             }
         });
         this.$store.commit('setTagsList', tagsList);
+        
+        util.ajax.interceptors.request.use(
+            config => {
+                config.headers.token = Cookies.get('token');
+                return config;
+            },
+            err => {
+                return Promise.reject(err);
+            }
+        );
+        util.ajax.interceptors.response.use(
+            res => {
+                return res;
+            },
+            err => {
+                if (err.response) {
+                    switch (err.response.status) {
+                        case 401:
+                            Cookies.remove('user');
+                            Cookies.remove('token');
+                            this.router.replace(loginRouter);
+                        default:
+                            this.router.replace(page500);
+                    }
+                }
+                return Promise.reject(err);
+            }
+        );
     }
 });
